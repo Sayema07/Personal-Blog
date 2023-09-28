ef16ad3541b0f18fd27d5181d286bbf67e90d153
Sayema07 <sayema.bucse.07@gmail.com>
2023-09-28 23:27:11 +0600

    Initial commit

---
+++ admin/inc/footer.php
@@ -0,0 +1,12 @@
+<div class="clear">
+        </div>
+    </div>
+    <div class="clear">
+    </div>
+    <div id="site_info">
+        <p>
+         &copy; Copyright <a href="https://github.com/Ashik763">Ashik's Roughkhata</a>. All Rights Reserved.
+        </p>
+    </div>
+</body>
+</html>
